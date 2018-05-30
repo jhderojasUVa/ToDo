47a443deaa2226f573820c30db76b4e6cc643d56
Jesus A Hernandez de Rojas <jesusangel.hernandez@uva.es>
2018-05-30 09:27:36 +0200

    Let's create the form (basic of course)

--- react/src/components/formcomponent/form.js
+++ react/src/components/formcomponent/form.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 
 class FormComponent extends Component {
+
+  constructor(props) {
+    super (props);
+    this.state = {
+      whattodo: ''
+    }
+
+    this.handleWrite = this.handleWrite.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleWrite(event) {
+    this.setState({
+      whattodo: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    alert('Submited '+this.state.whattodo);
+    event.preventDefault;
+  }
+
   render() {
     return (
       <div className="FormComponent">
-        Formcomponent
+        <p>I want to do {this.state.whattodo}</p>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="whattodo" value={this.state.whattodo} onChange={this.handleWrite}/> <input type="submit" value="Add me!" />
+        </form>
       </div>
     );
   }
